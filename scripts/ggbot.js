6c8b7f35cfab366e736d6cbe4efe119e99022d9c
Jackson Isted <dev@hawks008.dyndns.org>
2014-08-05 08:24:32 +1000

    wat.

--- scripts/ggbot.js
+++ scripts/ggbot.js
@@ -1,14 +1,22 @@
-# Description:
-#  GGGGGGGGGG
-#
-# Commands:
-#  gg - GG
-#
+// Description:
+//  GGGGGGGGGG
+//
+// Commands:
+//  gg - GG
+//  wat - watman!
+//
 
 module.exports = function(robot) { // Welcome to hubot xD
     
     //GG!
-    robot.hear(/gg/i, function(msg){
+    robot.hear(/(.*) gg(.*)/i, function(msg){
         msg.send("http://youtu.be/5es0NNtSNCU");
     });
+	
+	//wat
+	robot.hear(/wat/i, function(msg){
+		msg.send("http://www-static.weddingbee.com/pics/292233/Wat8.jpg")
+	})
+	
+	
 }
