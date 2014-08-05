7c2d9f44f2a1ee50867a8112b2f1ddadaeb45550
Jackson Isted <dev@hawks008.dyndns.org>
2014-08-05 12:35:07 +1000

    hmmm i wonder

--- scripts/mcUtils.js
+++ scripts/mcUtils.js
@@ -2,7 +2,7 @@
 //   hubot behead <player> - get a link to the players head
 
 module.exports = function(robot) {
-	robot.respond(/behead (.*)/i, function(msg){
-		msg.reply("http://cravatar.eu/helmhead/"+msg.match[1]+"/600.png");
+	robot.respond(/behead \b(.*)\b/i, function(msg){
+		msg.reply("http://cravatar.eu/helmhead/"+msg.match[1].trim()+"/600.png");
 	});
 }
