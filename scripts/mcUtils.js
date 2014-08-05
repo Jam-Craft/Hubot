b2762480cb7c6ed58881862ec55103c560a1914a
Jackson Isted <dev@hawks008.dyndns.org>
2014-08-05 12:09:15 +1000

    Start with mcutils

---
+++ scripts/mcUtils.js
@@ -0,0 +1,8 @@
+// Commands:
+//   hubot behead <player> - get a link to the players head
+
+module.exports = function(robot) {
+	robot.respond(/behead (?:[a-z,0-9]+)/i, function(msg){
+		msg.reply("http://cravatar.eu/helmhead/"+msg.match[1]+"/600.png");
+	});
+}
