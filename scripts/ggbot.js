ef7ed4230f4b6224762bfaf100c958ee844132b2
Jackson Isted <dev@hawks008.dyndns.org>
2014-10-04 12:13:21 +1000

    Revert "fix ggbot for new bot in channel"
    This reverts commit efb925b6370fa8028f78c85d4e8b3aa0cd9066d0.

--- scripts/ggbot.js
+++ scripts/ggbot.js
@@ -7,33 +7,32 @@
 //  
 //
 
-module.exports = function (robot) { // Welcome to hubot xD
-
-    var isBot = function (user) {
-        return user.name === "Inumuta"
-    };
-
+module.exports = function(robot) { // Welcome to hubot xD
+    
     //GG!
-    robot.hear(/\bgg\b/i, function (msg) {
-        if (!isBot(msg.user))
-            msg.send("http://youtu.be/5es0NNtSNCU");
+    robot.hear(/\bgg\b/i, function(msg){
+        msg.send("http://youtu.be/5es0NNtSNCU");
     });
-
-    //wat
-    robot.hear(/\bwat\b/i, function (msg) {
-        if (!isBot(msg.user))
-            msg.send("http://www-static.weddingbee.com/pics/292233/Wat8.jpg")
+	
+	//wat
+	robot.hear(/\bwat\b/i, function(msg){
+		msg.send("http://www-static.weddingbee.com/pics/292233/Wat8.jpg")
+	});
+	
+    robot.hear(/.*((slain by)|(killed by)|(blew up)|(pummeled by)|(drowned)|(died)|(went up in)|(burned to )|(tried to swim in)|(suffocated in a )|(starved to )|(was pricked to)|(hit the ground too)|(fell out of the)|(was shot by)).*/i,function(msg){
+		if(msg.user.name === "PoppysServer"){
+			msg.send("GGGGGG");
+		}
     });
-
-    robot.hear(/.*((slain by)|(killed by)|(blew up)|(pummeled by)|(drowned)|(died)|(went up in)|(burned to )|(tried to swim in)|(suffocated in a )|(starved to )|(was pricked to)|(hit the ground too)|(fell out of the)|(was shot by)).*/i, function (msg) {
-        if (msg.user.name === "PoppysServer") {
-            msg.send("GGGGGG");
-        }
-    });
-
-    //sleepy
-    robot.hear(/\b(bed|sleep|sleepy)\b/i, function (msg) {
-        msg.reply("http://img1.wikia.nocookie.net/__cb20110118013105/glee/images/a/aa/SLEEP_IS_FOR_THE_WEAK.jpg");
-    });
-
-};
+	
+	//sleepy
+	robot.hear(/\b(bed|sleep|sleepy)\b/i, function(msg){
+		msg.reply("http://img1.wikia.nocookie.net/__cb20110118013105/glee/images/a/aa/SLEEP_IS_FOR_THE_WEAK.jpg");
+	});
+	
+	//destory the eagle
+	robot.hear(/\b(brendon)\b/i, function(msg){
+		msg.reply("You know eagle_gunner isn't a god, right?");
+	});
+	
+}
